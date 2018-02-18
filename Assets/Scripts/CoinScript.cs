e1d098bd5db4ad063874a9836234074761b229f9
jobinjj <jobinj1048@gmail.com>
2018-02-18 12:39:30 -0800

    first commit

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy(gameObject);
+		score = score + 1;
+		Debug.Log(score);
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5f;
+	private float movement = 0f;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxis ("Horizontal");
+		Debug.Log (movement); 
+		rigidBody.velocity = new Vector2(movement*speed,rigidBody.velocity.y);
+	}
+}
